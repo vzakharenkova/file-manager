4e03534a619244a318e14039e191ca5defe8689b
vzakharenkova <v.s.zakharenkova@gmail.com>
2022-12-08 16:46:09 +0300

    feat: implement file operations commands

--- handlers/commandHandler.js
+++ handlers/commandHandler.js
@@ -2,6 +2,7 @@ import { getCurrentLocationMsg } from '../shared/messages.js';
 import path from 'path';
 
 import { nwd } from './nwd.js';
+// import { fileOperations } from './fileOperations.js';
 
 export const commandHandler = async (
   readLine,
@@ -45,6 +46,115 @@ export const commandHandler = async (
 
     case input === 'ls': {
       await nwd('ls', writeLocationMsg, askForCommand);
+
+      break;
+    }
+
+    case input.startsWith('cat '): {
+      const argsStr = input.slice(4);
+      const child_process = await import('child_process');
+      const cp = child_process.fork(
+        path.join(dirname, '/handlers/fileOperations.js'),
+        ['cat', argsStr]
+      );
+
+      cp.on('exit', () => {
+        writeLocationMsg();
+        askForCommand();
+      });
+
+      //   fileOperations(
+      //     'cat',
+      //     writeLocationMsg,
+      //     askForCommand,
+      //     readLine,
+      //     pathToFile
+      //   );
+
+      break;
+    }
+
+    case input.startsWith('add '): {
+      const fileName = input.slice(4);
+
+      const child_process = await import('child_process');
+      const cp = child_process.fork(
+        path.join(dirname, '/handlers/fileOperations.js'),
+        ['add', fileName]
+      );
+
+      cp.on('exit', () => {
+        writeLocationMsg();
+        askForCommand();
+      });
+
+      break;
+    }
+
+    case input.startsWith('rn '): {
+      const argsStr = input.slice(3);
+
+      const child_process = await import('child_process');
+      const cp = child_process.fork(
+        path.join(dirname, '/handlers/fileOperations.js'),
+        ['rn', argsStr]
+      );
+
+      cp.on('exit', () => {
+        writeLocationMsg();
+        askForCommand();
+      });
+
+      break;
+    }
+
+    case input.startsWith('cp '): {
+      const argsStr = input.slice(3);
+
+      const child_process = await import('child_process');
+      const cp = child_process.fork(
+        path.join(dirname, '/handlers/fileOperations.js'),
+        ['cp', argsStr]
+      );
+
+      cp.on('exit', () => {
+        writeLocationMsg();
+        askForCommand();
+      });
+
+      break;
+    }
+
+    case input.startsWith('mv '): {
+      const argsStr = input.slice(3);
+
+      const child_process = await import('child_process');
+      const cp = child_process.fork(
+        path.join(dirname, '/handlers/fileOperations.js'),
+        ['mv', argsStr]
+      );
+
+      cp.on('exit', () => {
+        writeLocationMsg();
+        askForCommand();
+      });
+
+      break;
+    }
+
+    case input.startsWith('rm '): {
+      const argsStr = input.slice(3);
+
+      const child_process = await import('child_process');
+      const cp = child_process.fork(
+        path.join(dirname, '/handlers/fileOperations.js'),
+        ['rm', argsStr]
+      );
+
+      cp.on('exit', () => {
+        writeLocationMsg();
+        askForCommand();
+      });
 
       break;
     }
--- shared/utils.js
+++ shared/utils.js
@@ -6,3 +6,24 @@ export const getDirname = (url) => {
 };
 
 export const getFilename = (url) => fileURLToPath(url);
+
+export const resolveTwoArgs = (argsStr) => {
+  let path_1;
+  let path_2;
+  if (!argsStr.includes("'") || !argsStr.includes('"')) {
+    const arr = argsStr.split(' ');
+    if (arr.length < 2) {
+      process.stdout.write('You should pass 2 args');
+      process.exit();
+    }
+    path_1 = path.resolve(arr[0]);
+    path_2 = path.resolve(arr[1]);
+  }
+  // else {
+  //     if (argsStr.startsWith("'") || argsStr.startsWith('"')) {
+
+  //     }
+  // }
+
+  return [path_1, path_2];
+};
